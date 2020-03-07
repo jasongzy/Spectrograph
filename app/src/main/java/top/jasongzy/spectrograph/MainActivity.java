4db3ecd23189392c259d382b0695874cd6ea036b
Jason Guo <gzysun1125@126.com>
2020-03-08 01:06:01 +0800

    Addfunc: spectrum plot

--- app/src/main/java/top/jasongzy/spectrograph/MainActivity.java
+++ app/src/main/java/top/jasongzy/spectrograph/MainActivity.java
@@ -1,19 +1,15 @@
 package top.jasongzy.spectrograph;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-import androidx.core.os.EnvironmentCompat;
-
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -27,13 +23,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.core.os.EnvironmentCompat;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     // 申请相机权限的requestCode
     private static final int PERMISSION_CAMERA_REQUEST_CODE = 0x00000012;
     private ImageView ivPhoto;
+    private ImageView plot;
     private TextView picResult;
     private FloatingActionButton fab;
     /**
@@ -68,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ivPhoto = findViewById(R.id.ivPhoto);
+        plot = findViewById(R.id.plot);
         picResult = findViewById(R.id.picResult);
         fab = findViewById(R.id.fab);
 
@@ -145,6 +153,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // 获取图片亮度值
+    public double[] getBright(Bitmap bm) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        int r, g, b;
+        List<Double> bright = new ArrayList<>();
+        for (int i = 0; i < width; i++) {
+            int localTemp = bm.getPixel(i, height / 2);
+            r = (localTemp | 0xff00ffff) >> 16 & 0x00ff;
+            g = (localTemp | 0xffff00ff) >> 8 & 0x0000ff;
+            b = (localTemp | 0xffffff00) & 0x0000ff;
+            // 灰度值计算公式
+            bright.add(0.299 * r + 0.587 * g + 0.114 * b);
+        }
+        // List 转 double 数组
+        Double[] DoubleArray = new Double[bright.size()];
+        bright.toArray(DoubleArray);
+        if (DoubleArray == null) {
+            return null;
+        }
+        double[] brightArray = new double[DoubleArray.length];
+        for (int i = 0; i < DoubleArray.length; i++) {
+            brightArray[i] = DoubleArray[i].doubleValue();
+        }
+        return brightArray;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -166,14 +201,43 @@ public class MainActivity extends AppCompatActivity {
                     ivPhoto.setImageBitmap(BitmapFactory.decodeFile(mCameraImagePath));
                 }
                 // 开始图像处理
-                picResult.setText("检测结果如下\n");
+                double[] brightArray = getBright(BitmapFactory.decodeFile(mCameraImagePath));
+                picResult.setText("检测结果如下\n\n");
                 picResult.append("苹果糖度：");
+                plotsSpectrum(brightArray);
             } else {
                 mCameraImagePath = null;
                 Toast.makeText(this, "取消", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
+    private void plotsSpectrum(double[] brightArray) {
+        int width = brightArray.length;
+        int height = 256;
+        Bitmap.Config mConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bm = Bitmap.createBitmap(width, height, mConfig);
+        Canvas canvas = new Canvas(bm);
+        //canvas.drawColor(Color.parseColor("#666666"));
+        Paint paint = new Paint();
+        //paint.setColor(Color.GRAY);
+        paint.setARGB(255, 200, 15, 15);
+        //画笔的风格，就是边框（绘制的是空心的）
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        //绘制路径
+        Path path = new Path();
+        //从哪个点开始绘制
+        path.moveTo(0, (float) (height - brightArray[0]));
+        for (int i = 1; i < width; i++) {
+            //然后绘制到哪个点
+            path.lineTo(i, (float) (height - brightArray[i]));
+        }
+        //按路径绘制
+        canvas.drawPath(path, paint);
+        plot.setImageBitmap(bm);
+    }
+
 
     /**
      * 处理权限申请的回调。
@@ -282,10 +346,11 @@ public class MainActivity extends AppCompatActivity {
             case R.id.clear:
                 RecursionDeleteFile(getExternalFilesDir(Environment.DIRECTORY_PICTURES));
                 ivPhoto.setImageDrawable(getResources().getDrawable(R.drawable.apple));
+                plot.setImageDrawable(getResources().getDrawable(R.drawable.spectrum));
                 mCameraImagePath = null;
                 picResult.setText(null);
-                //Toast.makeText(this, "已清除照片拍摄缓存", Toast.LENGTH_SHORT).show();
-                Snackbar.make(this.findViewById(android.R.id.content), "已清除照片拍摄缓存", Snackbar.LENGTH_LONG).show();
+                //Toast.makeText(this, "照片拍摄缓存已清除", Toast.LENGTH_SHORT).show();
+                Snackbar.make(this.findViewById(android.R.id.content), "照片拍摄缓存已清除", Snackbar.LENGTH_LONG).show();
                 break;
             case R.id.about:
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jasongzy")));
