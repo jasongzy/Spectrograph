8f6100c6968ae7317204996bc4b2156435632752
Jason Guo <gzysun1125@126.com>
2020-03-08 16:29:13 +0800

    Addfunc: absorbance plot
    Rearrange code & fix bugs

--- app/src/main/java/top/jasongzy/spectrograph/MainActivity.java
+++ app/src/main/java/top/jasongzy/spectrograph/MainActivity.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.net.Uri;
@@ -43,15 +44,15 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
     // 拍照的requestCode
     private static final int CAMERA_REQUEST_CODE = 0x00000010;
     // 申请相机权限的requestCode
     private static final int PERMISSION_CAMERA_REQUEST_CODE = 0x00000012;
     private ImageView ivPhoto;
     private ImageView plot;
-    private TextView picResult;
+    private TextView finalResult;
     private FloatingActionButton fab;
+    private FloatingActionButton fabPlot;
     /**
      * 用于保存拍照图片的uri
      */
@@ -67,6 +68,22 @@ public class MainActivity extends AppCompatActivity {
      */
     private boolean isAndroidQ = Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
 
+    private String savedmCameraImagePath = null;
+    private boolean isAbsorbance = false;
+    private double[] brightArray = null;
+    private double[] savedBrightArray = null;
+    private double[] AData = null;
+    private Bitmap absorbancePlot = null;
+    private Bitmap spectrumPlot = null;
+
+    private void toastMessage(String text, boolean isLengthLong) {
+        if (isLengthLong) {
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,86 +91,153 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /**
+         * 原始图片
+         */
         ivPhoto = findViewById(R.id.ivPhoto);
-        plot = findViewById(R.id.plot);
-        picResult = findViewById(R.id.picResult);
-        fab = findViewById(R.id.fab);
+        ivPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCameraImagePath == null) {
+                    toastMessage("找个苹果试试看吧！", false);
+                } else {
+                    toastMessage("图片路径：" + mCameraImagePath, true);
+                }
+            }
+        });
+        ivPhoto.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                toastMessage("长按相机按钮可以从相册选择图片哦", true);
+                return true;
+            }
+        });
 
+        /**
+         * 绘制的曲线
+         */
+        plot = findViewById(R.id.plot);
+        plot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickPlot();
+            }
+        });
+        plot.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if (mCameraImagePath != null) {
+                    if (isAbsorbance) {
+                        toastMessage("点击悬浮按钮可切换光谱/吸光度曲线", false);
+                    } else {
+                        toastMessage("点击可设置当前数据为「光源光谱」", false);
+                    }
+                }
+                return true;
+            }
+        });
+
+        /**
+         * 相机按钮
+         */
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                savedmCameraImagePath = mCameraImagePath;
                 checkPermissionAndCamera();
             }
         });
         fab.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
+                savedmCameraImagePath = mCameraImagePath;
                 checkPermissionAndAlbum();
                 return true;
             }
         });
-        ivPhoto.setOnClickListener(new View.OnClickListener() {
+
+        /**
+         * 图像对比按钮
+         */
+        fabPlot = findViewById(R.id.fabPlot);
+        fabPlot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                clickImage();
+                clickFabPlot();
             }
         });
-        ivPhoto.setOnLongClickListener(new View.OnLongClickListener() {
+        fabPlot.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                longPressImage();
+                if (absorbancePlot == null) {
+                    toastMessage("绘制吸光度曲线并测量苹果糖度", false);
+                } else {
+                    toastMessage("切换光谱/吸光度曲线", false);
+                }
                 return true;
             }
         });
+
+        finalResult = findViewById(R.id.finalResult);
     }
 
-    private void checkPermissionAndAlbum() {
-        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
-                Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    private void clickFabPlot() {
+        if (absorbancePlot == null) {
+            Snackbar.make(this.findViewById(android.R.id.content), "是否要绘制「当前光谱」相对于「光源光谱」的吸光度曲线？", Snackbar.LENGTH_LONG)
+                    .setAction("是的", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (savedBrightArray.length == brightArray.length) {
+                                //画吸光度曲线图
+                                absorbancePlot = plotAbsorbance(brightArray, savedBrightArray);
+                                plot.setImageBitmap(absorbancePlot);
+                                isAbsorbance = true;
+                                printResult();
+                            } else {
+                                toastMessage("图像像素不匹配！", true);
+                            }
+                        }
+                    }).show();
         } else {
-            //没有权限，申请权限。
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    PERMISSION_CAMERA_REQUEST_CODE);
+            //切换
+            if (isAbsorbance) {
+                plot.setImageBitmap(spectrumPlot);
+                Toast.makeText(this, "光谱曲线", Toast.LENGTH_SHORT).show();
+                isAbsorbance = false;
+            } else {
+                plot.setImageBitmap(absorbancePlot);
+                Toast.makeText(this, "吸光度曲线", Toast.LENGTH_SHORT).show();
+                isAbsorbance = true;
+            }
         }
     }
 
-    private void clickImage() {
-        if (mCameraImagePath == null) {
-            Toast.makeText(this, "找个苹果试试看吧！", Toast.LENGTH_LONG).show();
-            ;
-        } else {
-            Toast.makeText(this, "图片路径：" + mCameraImagePath, Toast.LENGTH_LONG).show();
-            ;
+    private void clickPlot() {
+        if (mCameraImagePath != null) {
+            if (isAbsorbance) {
+                Toast.makeText(this, "点击悬浮按钮可切换光谱/吸光度曲线", Toast.LENGTH_SHORT).show();
+            } else {
+                Snackbar.make(this.findViewById(android.R.id.content), "是否设置当前数据为光源光谱？", Snackbar.LENGTH_LONG)
+                        .setAction("是的", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                savedBrightArray = new double[brightArray.length];
+                                for (int i = 0; i < brightArray.length; i++) {
+                                    savedBrightArray[i] = brightArray[i];
+                                }
+                                fabPlot.setVisibility(View.VISIBLE);
+                                absorbancePlot = null;
+                                toastMessage("已设置", false);
+                            }
+                        }).show();
+            }
         }
-    }
-
-    private void longPressImage() {
-        Toast.makeText(this, "长按相机按钮可以从相册选择图片哦", Toast.LENGTH_LONG).show();
-        Vibrator vibrator = (Vibrator) this.getSystemService(this.VIBRATOR_SERVICE);
-        vibrator.vibrate(50);
     }
 
     /**
-     * 检查权限并拍照。
-     * 调用相机前先检查权限。
+     * 获取图片亮度（灰度）值
      */
-    private void checkPermissionAndCamera() {
-        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
-                Manifest.permission.CAMERA);
-        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
-            //有权限，调起相机拍照。
-            openCamera();
-        } else {
-            //没有权限，申请权限。
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
-                    PERMISSION_CAMERA_REQUEST_CODE);
-        }
-    }
-
-    // 获取图片亮度值
     public double[] getBright(Bitmap bm) {
         int width = bm.getWidth();
         int height = bm.getHeight();
@@ -167,7 +251,7 @@ public class MainActivity extends AppCompatActivity {
             // 灰度值计算公式
             bright.add(0.299 * r + 0.587 * g + 0.114 * b);
         }
-        // List 转 double 数组
+        // Double List 转 double 数组
         Double[] DoubleArray = new Double[bright.size()];
         bright.toArray(DoubleArray);
         if (DoubleArray == null) {
@@ -180,39 +264,10 @@ public class MainActivity extends AppCompatActivity {
         return brightArray;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                if (isAndroidQ) {
-                    // Android 10 使用图片uri加载
-                    ivPhoto.setImageURI(mCameraUri);
-                } else {
-                    // 使用图片路径加载
-                    try {
-                        Cursor cursor = getContentResolver().query(data.getData(), null, null, null, null);
-                        if (cursor != null && cursor.moveToFirst()) {
-                            mCameraImagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
-                        }
-                    } catch (Exception e) {
-                        //e.printStackTrace();
-                    }
-                    ivPhoto.setImageBitmap(BitmapFactory.decodeFile(mCameraImagePath));
-                }
-                // 开始图像处理
-                double[] brightArray = getBright(BitmapFactory.decodeFile(mCameraImagePath));
-                picResult.setText("检测结果如下\n\n");
-                picResult.append("苹果糖度：");
-                plotsSpectrum(brightArray);
-            } else {
-                mCameraImagePath = null;
-                Toast.makeText(this, "取消", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void plotsSpectrum(double[] brightArray) {
+    /**
+     * 绘制光谱曲线
+     */
+    private Bitmap plotSpectrum(double[] brightArray) {
         int width = brightArray.length;
         int height = 256;
         Bitmap.Config mConfig = Bitmap.Config.ARGB_8888;
@@ -235,9 +290,138 @@ public class MainActivity extends AppCompatActivity {
         }
         //按路径绘制
         canvas.drawPath(path, paint);
-        plot.setImageBitmap(bm);
+        return bm;
     }
 
+    /**
+     * 绘制吸光度曲线
+     */
+    private Bitmap plotAbsorbance(double[] brightArray, double[] savedBrightArray) {
+        AData = new double[savedBrightArray.length];
+        double dataMax = 0;
+        for (int i = 0; i < savedBrightArray.length; i++) {
+            if (savedBrightArray[i] == 0) {
+                savedBrightArray[i] += 1;
+            }
+            if (brightArray[i] == 0) {
+                brightArray[i] += 1;
+            }
+            AData[i] = Math.log10(savedBrightArray[i] / brightArray[i]);
+            if (dataMax < AData[i]) {
+                dataMax = AData[i];
+            }
+            if (AData[i] < 0) {
+                AData[i] = 0;
+            }
+        }
+        int width = savedBrightArray.length;
+        int height = (dataMax != 0) ? (int) Math.ceil(dataMax) * 100 : 256;
+        Bitmap.Config mConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bm = Bitmap.createBitmap(width, height, mConfig);
+        Canvas canvas = new Canvas(bm);
+        //canvas.drawColor(Color.parseColor("#666666"));
+        Paint paint = new Paint();
+        paint.setColor(Color.GRAY);
+        //paint.setARGB(255, 200, 15, 15);
+        //画笔的风格，就是边框（绘制的是空心的）
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+        //绘制路径
+        Path path = new Path();
+        //从哪个点开始绘制
+        path.moveTo(0, (float) (height - 100 * AData[0]));
+        for (int i = 1; i < width; i++) {
+            //然后绘制到哪个点
+            path.lineTo(i, (float) (height - 100 * AData[i]));
+        }
+        //按路径绘制
+        canvas.drawPath(path, paint);
+        return bm;
+    }
+
+    /**
+     * 打印糖度检测结果
+     */
+    private void printResult() {
+        Vibrator vibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(50);
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        vibrator.vibrate(50);
+        finalResult.setText("检测结果如下\n\n");
+        finalResult.append("苹果糖度：");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                if (isAndroidQ) {
+                    // Android 10 使用图片uri加载
+                    ivPhoto.setImageURI(mCameraUri);
+                } else {
+                    // 使用图片路径加载
+                    try {
+                        Cursor cursor = getContentResolver().query(data.getData(), null, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            mCameraImagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+                        }
+                    } catch (Exception e) {
+                        //e.printStackTrace();
+                    }
+                    ivPhoto.setImageBitmap(BitmapFactory.decodeFile(mCameraImagePath));
+                }
+                finalResult.setText(null);
+                // 开始图像处理
+                brightArray = getBright(BitmapFactory.decodeFile(mCameraImagePath));
+                spectrumPlot = plotSpectrum(brightArray);
+                plot.setImageBitmap(spectrumPlot);
+                isAbsorbance = false;
+                absorbancePlot = null;
+            } else {
+                mCameraImagePath = savedmCameraImagePath;
+                Toast.makeText(this, "取消", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    /**
+     * 检查权限并拍照。
+     * 调用相机前先检查权限。
+     */
+    private void checkPermissionAndCamera() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.CAMERA);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+            //有权限，调起相机拍照。
+            openCamera();
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    /**
+     * 检查权限并打开相册
+     */
+    private void checkPermissionAndAlbum() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PERMISSION_CAMERA_REQUEST_CODE);
+        }
+    }
 
     /**
      * 处理权限申请的回调。
@@ -334,6 +518,10 @@ public class MainActivity extends AppCompatActivity {
         return tempFile;
     }
 
+
+    /**
+     * toolbar按钮
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
@@ -344,11 +532,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.clear:
+                //删除app拍摄的照片
                 RecursionDeleteFile(getExternalFilesDir(Environment.DIRECTORY_PICTURES));
+                //布局还原
                 ivPhoto.setImageDrawable(getResources().getDrawable(R.drawable.apple));
                 plot.setImageDrawable(getResources().getDrawable(R.drawable.spectrum));
+                fabPlot.setVisibility(View.INVISIBLE);
+                finalResult.setText(null);
+                //清除变量
                 mCameraImagePath = null;
-                picResult.setText(null);
+                savedmCameraImagePath = null;
+                isAbsorbance = false;
+                brightArray = null;
+                savedBrightArray = null;
+                AData = null;
+                absorbancePlot = null;
+                spectrumPlot = null;
                 //Toast.makeText(this, "照片拍摄缓存已清除", Toast.LENGTH_SHORT).show();
                 Snackbar.make(this.findViewById(android.R.id.content), "照片拍摄缓存已清除", Snackbar.LENGTH_LONG).show();
                 break;
